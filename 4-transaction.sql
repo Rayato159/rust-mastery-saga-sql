97137be0330ae8c63eac75f3027f0fdfd44468fa
Ruangyot Nanchiang <Rayato159@gmail.com>
2024-11-21 23:04:33 +0700

    Update 4-transaction.sql

--- 4-transaction.sql
+++ 4-transaction.sql
@@ -1,12 +1,5 @@
--- Crabby wants to make multiple changes safely in case something goes wrong.
-BEGIN;
-    UPDATE adventurers SET level = level + 2 WHERE name = 'Shelldon';
-    INSERT INTO items (item_name, item_type, rarity, adventurer_id) VALUES ('Tidal Staff', 'Weapon', 'Epic', 2);
-    -- Oops! Let's roll back the changes.
-ROLLBACK;
-
--- Crabby tries again and this time commits.
-BEGIN;
-    UPDATE adventurers SET level = level + 2 WHERE name = 'Shelldon';
-    INSERT INTO items (item_name, item_type, rarity, adventurer_id) VALUES ('Tidal Staff', 'Weapon', 'Epic', 2);
-COMMIT;
+begin;
+	update adventurers set level = level + 2 where name = 'Shelldon';
+	insert into items (item_name, item_type, rarity, adventurer_id) 
+	values ('Tidal Staff', 'Weapon', 'Epic', 2);
+commit;
